e98184f878d7064bab0849cc558e87e3205a3693
Lanxre <tat4vit7@gmail.com>
2026-08-13 21:50:33 +0300

    chore: added nessary options

--- internal/spec.go
+++ internal/spec.go
@@ -37,14 +37,79 @@ func init() {
 		{
 			Name:  "dmp",
 			Short: "Dump database",
-			Options: map[string]*models.Definition{
-				"p": {
-					Name:     "p",
-					Short:    "Path to env file with database credentials",
+			Children: map[string]*models.Definition{
+				"postgres": {
+					Name:     "postgres",
+					Short:    "Dump PostgreSQL database",
 					Terminal: true,
+					Handler:  handlers.RunDmp,
+					Options: map[string]*models.Definition{
+						"env": {
+							Name:     "env",
+							Short:    "Path to env file with database credentials",
+							Terminal: true,
+						},
+						"o": {
+							Name:     "o",
+							Short:    "Output file path",
+							Terminal: true,
+						},
+						"f": {
+							Name:     "f",
+							Short:    "Output format (sql, json)",
+							Terminal: true,
+						},
+						"n": {
+							Name:     "n",
+							Short:    "Database name",
+							Terminal: true,
+						},
+						"p": {
+							Name:     "p",
+							Short:    "Database port",
+							Terminal: true,
+						},
+						"u": {
+							Name:     "u",
+							Short:    "User database",
+							Terminal: true,
+						},
+						"psw": {
+							Name:     "psw",
+							Short:    "Password",
+							Terminal: true,
+						},
+					},
+				},
+				"sqlite": {
+					Name:     "sqlite",
+					Short:    "Dump SQLite database",
+					Terminal: true,
+					Handler:  handlers.RunDmp,
+					Options: map[string]*models.Definition{
+						"env": {
+							Name:     "env",
+							Short:    "Path to env file with database credentials",
+							Terminal: true,
+						},
+						"o": {
+							Name:     "o",
+							Short:    "Output file path",
+							Terminal: true,
+						},
+						"f": {
+							Name:     "f",
+							Short:    "Output format (sql, json)",
+							Terminal: true,
+						},
+						"db": {
+							Name:     "db",
+							Short:    "Path to SQLite database file",
+							Terminal: true,
+						},
+					},
 				},
 			},
-			Handler: handlers.RunDmp,
 		},
 	}
 }
